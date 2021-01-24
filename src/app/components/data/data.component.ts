8a7612ab1d6779f803f3d7f43ae05840b64579f7
jsersan <jsersan@gmail.com>
2021-01-24 20:53:44 +0100

    first commit

---
+++ src/app/components/data/data.component.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
+import { Observable } from 'rxjs/Rx'
+
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styles: []
+})
+export class DataComponent {
+
+  forma:FormGroup;
+
+  usuario: Object = {
+
+    nombrecompleto: {
+      nombre: "Txema",
+      apellido: "Serrano"
+    },
+    correo: "jsersan@gmail.com",
+    pasatiempos: [ "Correr","Dormir","Comer" ]
+  }
+
+  constructor() {
+
+    console.log(this.usuario);
+
+    this.forma=new FormGroup({
+      'nombrecompleto': new FormGroup({
+
+          'nombre': new FormControl('',  [
+                                          Validators.required,
+                                          Validators.minLength(3)
+                                        ]),
+          'apellido': new FormControl('',Validators.required),
+    }),
+      'correo': new FormControl('',  [
+                                      Validators.required,
+                                      Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+                                    ]),
+    'pasatiempos': new FormArray([
+        new FormControl('Correr', Validators.required) // Valor por defecto: correr
+        ])
+    })
+
+    // this.forma.setValue(this.usuario);
+}
+
+
+  agregarPasatiempo(){
+    (<FormArray>this.forma.controls['pasatiempos']).push(
+      new FormControl('', Validators.required)
+    )
+  }
+
+  guardarCambios(){
+      console.log(this.forma.value)
+      console.log(this.forma)
+
+      this.forma.reset({
+        nombrecompleto:{
+          nombre:"",
+          apellido:""
+        },
+        correo:""
+    });
+  }
+}
